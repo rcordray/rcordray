22c8eafca16a912fad443d3a3a919fbe8fefc3b7
Robert Cordray <robertcordray@gmail.com>
2017-11-02 15:23:54 -0500

    missed a few src

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,16 @@ const sass = require('gulp-sass');
 
 // Compile Sass & Inject Into Browser
 gulp.task('sass', function() {
-    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
+    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'public/scss/*.scss'])
         .pipe(sass())
-        .pipe(gulp.dest("src/css"))
+        .pipe(gulp.dest("public/css"))
         .pipe(browserSync.stream());
 });
 
-// Move JS Files to src/js
+// Move JS Files to public/js
 gulp.task('js', function() {
     return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
-        .pipe(gulp.dest("src/js"))
+        .pipe(gulp.dest("public/js"))
         .pipe(browserSync.stream());
 });
 
@@ -28,13 +28,13 @@ gulp.task('serve', ['sass'], function() {
     gulp.watch("public/*.html").on('change', browserSync.reload);
 });
 
-// Move Fonts to src/fonts
+// Move Fonts to public/fonts
 gulp.task('fonts', function() {
     return gulp.src('node_modules/font-awesome/fonts/*')
         .pipe(gulp.dest('public/fonts'))
 })
 
-// Move Font Awesome CSS to src/css
+// Move Font Awesome CSS to public/css
 gulp.task('fa', function() {
     return gulp.src('node_modules/font-awesome/css/font-awesome.min.css')
         .pipe(gulp.dest('public/css'))
